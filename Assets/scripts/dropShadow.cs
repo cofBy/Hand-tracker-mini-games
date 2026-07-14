f65fc35b2fdc6bfa612df8d13dd70798abe1e4ae
cofBy <foxmagic24@gmail.com>
2026-07-14 10:05:36 +0300

    made a drop shadow effect
    a cool cartoony drop shadow

---
+++ Assets/scripts/dropShadow.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class dropShadow : MonoBehaviour
+{
+    [Header("spawning shadow")]
+    public Vector2 offset;
+    GameObject shadow;
+
+    [Header("solid color")]
+    public Material mat;
+
+    private void Start()
+    {
+        shadow = new GameObject("drop shadow");
+
+        shadow.transform.parent = transform;
+        shadow.transform.localScale = Vector3.one;
+
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        SpriteRenderer shadowRenderer = shadow.AddComponent<SpriteRenderer>();
+
+        shadowRenderer.sprite = renderer.sprite;
+        shadowRenderer.material = mat;
+        shadowRenderer.sortingLayerName = renderer.sortingLayerName;
+        shadowRenderer.sortingOrder = renderer.sortingOrder - 1;
+    }
+    private void Update()
+    {
+        shadow.transform.position = (Vector2)transform.position + offset;
+        shadow.transform.rotation = transform.rotation;
+    }
+}
