acd3897bb1328250374a4d3fc0e42e43de8829ee
cofBy <foxmagic24@gmail.com>
2026-07-21 20:03:32 +0300

    added movement
    the snake now moves slowly towards the palm until it reaches

--- Assets/scripts/playerSnake.cs
+++ Assets/scripts/playerSnake.cs
@@ -3,7 +3,16 @@ using UnityEngine;
 public class playerSnake : MonoBehaviour
 {
     [Header("movement")]
+    public sentisHandTracker handTracker;
     public Rigidbody2D rb;
+    public float speed;
+
+    [Header("camera movement")]
+    public Camera cam;
+    public float followTime;
+    Vector2 vel;
+
+    public float lookAhead;
 
     [Header("making the snake")]
     public GameObject snakeBodyPrefab;
@@ -14,6 +23,11 @@ public class playerSnake : MonoBehaviour
 
     private void Update()
     {
+        Vector2 dir = handTracker.palmCenter() - rb.position;
+        rb.linearVelocity = dir.normalized * speed;
+
+        handleCamera();
+
         if (oldSnakeLength != snakeLength)
         {
             int differance = snakeLength - oldSnakeLength;
@@ -22,9 +36,15 @@ public class playerSnake : MonoBehaviour
             {
                 oldSnakeLength += 1;
                 GameObject segmentInstance = PoolManager.spawnObject(snakeBodyPrefab, Vector3.zero, Quaternion.identity);
-                Point.segment bodySegment = new Point.segment { transform = segmentInstance.transform, distance = 1 };
+                Point.segment bodySegment = new Point.segment { transform = segmentInstance.transform, distance = 1f };
                 animator.Segments.Add(bodySegment);
             }
         }
+    }
+
+    void handleCamera()
+    {
+        Vector2 target = (Vector2)transform.position + (rb.linearVelocity.normalized * lookAhead);
+        cam.transform.position = (Vector3)Vector2.SmoothDamp(cam.transform.position, target, ref vel, followTime) + new Vector3(0, 0, -10f);
     }
 }
